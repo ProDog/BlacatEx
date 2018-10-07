734c780355dfd53874735cf52bef4387f0ea68b5
SatoshiGrip <xiuboard@foxmail.com>
2018-10-07 22:50:57 +0800

    增加地址生成功能

---
+++ GetAddress/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Nethereum.Hex.HexConvertors.Extensions;
+
+namespace GetAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ecKey = Nethereum.Signer.EthECKey.GenerateKey();
+            var privateKey = ecKey.GetPrivateKeyAsBytes().ToHex();
+            var account = new Nethereum.Web3.Accounts.Account(privateKey);
+
+            
+        }
+    }
+}
